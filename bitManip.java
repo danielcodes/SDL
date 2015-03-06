739f93263f5ac3698dafb66eca958efba023e476
Daniel Chia <daniel_1118@hotmail.com>
2015-03-06 00:45:13 -0800

    just starting

---
+++ bitManip.java
@@ -0,0 +1,13 @@
+public class bitManip
+{
+    public static void main(String[] args)
+    {
+        //assignments for bit manipulation
+        //function for recognizing an IP address in a network through a mask
+        //mixing of colors
+        System.out.println("MOAR HOMEWORK");
+
+
+    }
+
+}
