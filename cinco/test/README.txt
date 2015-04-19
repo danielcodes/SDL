c3a1230bdeeb2aa9f35d7e5a9c267f23eb9b680f
Daniel Chia <daniel_1118@hotmail.com>
2015-04-19 02:38:44 -0700

    Added clear button

---
+++ cinco/test/README.txt
@@ -0,0 +1,4 @@
+Progress log, to keep up with updates and tweaks
+
+-adding clear at the bottom, one large button
+--must add extra to allocate for the extra row
--- cinco/test/cal.java
+++ cinco/test/cal.java
@@ -8,25 +8,26 @@ public class cal extends JFrame implements ActionListener
 
     //Goal, a 5x5 gridded calculator
     // DISPLAY
-    // | 7 | 8 | 9 |   | / | MC |
-    // | 4 | 5 | 6 |   | x | MR |
-    // | 1 | 2 | 3 |   | - | M+ |
-    // | 0 | . | = |   | + | M- |
+    // | 7 | 8 | 9 | | / | MC |
+    // | 4 | 5 | 6 | | x | MR |
+    // | 1 | 2 | 3 | | - | M+ |
+    // | 0 | . | = | | + | M- |
 
     //5 rows
-    JPanel[] row = new JPanel[5];
+    JPanel[] row = new JPanel[6];
 
     //buttons
-    JButton[] button = new JButton[20];
+    JButton[] button = new JButton[21];
 
     //button labels
-    String[] buttonLabel = {"7", "8", "9", "/", "M+",
+    String[] buttonLabel = {"7", "8", "9", "/", "MC",
                              "4", "5", "6", "x", "MR",
                              "1", "2", "3", "-", "M+",
-                             ".", "0", "=", "+", "M-"};
+                             ".", "0", "=", "+", "M-",
+                             "CLEAR"};
 
     //size of buttons, only two regular and display
-    int[] width = {260, 50};
+    int[] width = {250, 50};
     int[] height = {35, 40};
 
     Dimension displayDim = new Dimension(width[0], height[0]);
@@ -49,10 +50,10 @@ public class cal extends JFrame implements ActionListener
     {
         super("Calculator");
         setDesign();
-        setSize(320, 290);
+        setSize(300, 300);
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        GridLayout grid = new GridLayout(5,5);
+        GridLayout grid = new GridLayout(6,5);
         setLayout(grid);
 
         //init operations
@@ -66,7 +67,7 @@ public class cal extends JFrame implements ActionListener
         FlowLayout f2 = new FlowLayout(FlowLayout.CENTER,1,1);
 
         //fill row array and fill it with the created layouts
-        for(int i = 0; i < 5; i++)
+        for(int i = 0; i < 6; i++)
         {
             row[i] = new JPanel();
         }
@@ -74,13 +75,13 @@ public class cal extends JFrame implements ActionListener
         //first
         row[0].setLayout(f1);
         //subsequent rows
-        for(int i = 1; i < 5; i++)
+        for(int i = 1; i < 6; i++)
         {
             row[i].setLayout(f2);
         }
 
         //init buttons
-        for(int i = 0; i < 20; i++)
+        for(int i = 0; i < 21; i++)
         {
             button[i] = new JButton();
             button[i].setText(buttonLabel[i]);
@@ -121,6 +122,11 @@ public class cal extends JFrame implements ActionListener
             row[4].add(button[i]);
         add(row[4]);
 
+        //adding in the clear button
+        button[20].setPreferredSize(displayDim);
+        row[5].add(button[20]);
+        add(row[5]);
+
         setVisible(true);
     }
 
@@ -145,15 +151,8 @@ public class cal extends JFrame implements ActionListener
 
 
 
-
-
-
-
-
-
     public static void main (String[] args)
     {
         cal c = new cal();
-
     }
 }
