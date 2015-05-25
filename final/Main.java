0e1ae644b0c7115e30bacc0a09e9bb88db1c566d
Daniel Chia <daniel_1118@hotmail.com>
2015-05-25 03:02:13 -0700

    learning GUI

--- final/Main.java
+++ final/Main.java
@@ -1,10 +1,12 @@
 //Simple Information Management System for CCNY
 
+import javax.swing.*;
+
 public class Main
 {
     public static void main(String[] args)
     {
         System.out.println("hello");
-
+        String msg = JOptionPane.showInputDialog("hello sir");
     }
 }
